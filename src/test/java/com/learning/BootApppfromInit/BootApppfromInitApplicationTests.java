3de5c9823ae0a181522f58a82e27e586ca7bc591
ashish <ashishkh300@gmail.com>
2020-07-24 10:21:27 +0530

    init

---
+++ src/test/java/com/learning/BootApppfromInit/BootApppfromInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.BootApppfromInit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootApppfromInitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
